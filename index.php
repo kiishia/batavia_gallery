7e43d9ced0a011828e014b0ab2ddfea50bc4c600
kiishia <kiishia@hotmail.com>
2021-06-04 12:54:28 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?><section class="container-fluid aboutbg">
+      
+      <div class="container">
+          
+          
+          
+         
+          
+          <div class="col-md-6 text-center">
+              <h2> Gallery </h2>
+          
+          <p class="tickets"> Tickets are now on sale for our upcoming exhibition on the Batavia Shipwreck of 1929
+Opens 23 April. 
+          <p class="tickets">Tickets $10 </p>
+<p class="tickets">Members/under 16s free. </p>
+              
+              <img class="img-responsive book" src="images/book%20now.JPG">
+              
+                </div>
+       
+              <div class="col-md-6">
+              <img class="img-responsive" src="images/gallery.JPG">
+              
+            
+          
+          </div>
+       </div>
+      
+      </section>
+  
+      
+<!--      NEW SECTION-->
+      
+      
+      
+      <section class="container-fluid bottom">
+      <div class="container">
+        
+              
+          
+              <h3>What's on WA Shipwrecks</h3>
+              
+             
+<!--          div class row ending-->
+          
+          
+          <div class="col-md-6">
+        <img class="img-responsive" src="images/tours.jpg">
+              
+          </div>
+          
+              
+          <div class="col-md-6">
+              <p class="tour">Make the most of your visit to the WA Shipwrecks Museum with a free tour.</p>
+
+<p class="tour">Bookings are essential and can be made by calling 1300 134 081 or emailing reception@museum.wa.gov.au </p>
+
+<p class="tour">Site accessibility information can be found on the main WA Shipwrecks Museum page.</p>
+             
+              
+              <h4>School trips and tours</h4>
+              
+          </div>
+          
+          </div>
+<!--          container ending -->
+      
+      </section><?php get_footer(); /* Tells WordPress to include footer.php */ ?>
