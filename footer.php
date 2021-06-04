e568bd1cdf1ec4018b597dc7ce68336184d75c8f
kiishia <kiishia@hotmail.com>
2021-06-04 12:54:08 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="container-fluid">
+      
+          <div class="footer">
+          
+          <img class="img-responsive footer" src="images/footer.JPG">
+          
+          
+          </div>
+<!--      end of footer div-->
+      
+      </footer>
+    
+  </body>
+</html>
+<?php get_footer(); ?>
